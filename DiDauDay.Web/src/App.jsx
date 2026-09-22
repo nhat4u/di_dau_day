51e55e3b6f0eb031386cf7fa35f8195c2a7a680a
nhat4u <minhnhatxd0307@gmail.com>
2026-09-22 11:05:25 +0700

    Update App.jsx

--- DiDauDay.Web/src/App.jsx
+++ DiDauDay.Web/src/App.jsx
@@ -6,10 +6,21 @@ import {
 } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import HomestaysPage from './pages/HomestaysPage'
 import HomestayDetailPage from './pages/HomestayDetailPage'
 import MyBookingsPage from './pages/MyBookingsPage'
 import OwnerBookingsPage from './pages/OwnerBookingsPage'
 import WalletPage from './pages/WalletPage'
+import AdminRefundsPage from './pages/AdminRefundsPage'
+import OwnerHomestaysPage from './pages/OwnerHomestaysPage'
+import OwnerHomestayEditorPage from './pages/OwnerHomestayEditorPage'
+import AdminHomestayChangesPage from './pages/AdminHomestayChangesPage'
+import AdminOwnersPage from './pages/AdminOwnersPage'
+import OwnerProfilePage from './pages/OwnerProfilePage'
+import AdminDashboardPage from './pages/AdminDashboardPage'
+import OwnerDashboardPage from './pages/OwnerDashboardPage'
+import AdminBookingsPage from './pages/AdminBookingsPage'
 
 function App() {
   return (
@@ -17,6 +28,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/homestays" element={<HomestaysPage />} />
         <Route
           path="/homestays/:slug"
           element={<HomestayDetailPage />}
@@ -29,7 +42,51 @@ function App() {
           path="/owner/bookings"
           element={<OwnerBookingsPage />}
         />
+        <Route
+          path="/owner/homestays"
+          element={<OwnerHomestaysPage />}
+        />
+        <Route
+          path="/owner/homestays/new"
+          element={<OwnerHomestayEditorPage />}
+        />
+        <Route
+          path="/owner/homestays/:id/setup"
+          element={<OwnerHomestayEditorPage />}
+        />
+        <Route
+          path="/owner/homestays/:id/change"
+          element={<OwnerHomestayEditorPage mode="change-request" />}
+        />
+        <Route
+          path="/owner/profile"
+          element={<OwnerProfilePage />}
+        />
         <Route path="/wallet" element={<WalletPage />} />
+        <Route
+          path="/owner/dashboard"
+          element={<OwnerDashboardPage />}
+        />
+        <Route
+          path="/admin/dashboard"
+          element={<AdminDashboardPage />}
+        />
+        <Route
+          path="/admin/bookings"
+          element={<AdminBookingsPage />}
+        />
+        <Route
+          path="/admin/refunds"
+          element={<AdminRefundsPage />}
+        />
+        <Route
+          path="/admin/homestay-changes"
+          element={<AdminHomestayChangesPage />}
+        />
+        <Route
+          path="/admin/owners"
+          element={<AdminOwnersPage />}
+        />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
