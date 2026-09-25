abd2c79f445cd7000e074494280b638b2ff7e248
nhat4u <minhnhatxd0307@gmail.com>
2026-09-25 10:46:26 +0700

    Add files via upload

---
+++ DiDauDay.Web/src/styles/OwnerProfile.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace DiDauDay.Api.Models;
+
+public partial class OwnerProfile
+{
+    public uint Id { get; set; }
+
+    public uint UserId { get; set; }
+
+    public string CitizenId { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string? BankName { get; set; }
+
+    public string? BankAccount { get; set; }
+
+    public string? BankAccountName { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
